13459ae7bb94aa198b2f80885f1be088c6d1e589
Jeffrey Liu <jeffreyliu8@gmail.com>
2018-11-26 14:50:18 -0800

    add camera2video

--- app/src/main/java/com/askjeffreyliu/facedancerevolution/MainActivity.kt
+++ app/src/main/java/com/askjeffreyliu/facedancerevolution/MainActivity.kt
@@ -7,6 +7,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_camera)
+        if (null == savedInstanceState) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, Camera2VideoFragment.newInstance())
+                .commit()
+        }
     }
 }
